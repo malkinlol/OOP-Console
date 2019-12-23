d8a5660ed358c289e35d85df9a3429e5485cc29b
Eduard Popkov <E.Papkou@sam-solutions.com>
2019-12-23 19:45:18 +0300

    feat: Lab2_Option2 LOCALFUNCTION

---
+++ Lab2_Option2/LOCALFUNCTION/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOCALFUNCTION
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (int, int, int, char) LocalFunction(int[] array, string str)
+            {
+                int max = array.Max();
+                int min = array.Min();
+                int sum = array.Sum();
+                char firstLetter = str[0];
+
+                return (max, min, sum, firstLetter);
+            }
+
+            var result = LocalFunction(new int[] { 1, 2, 3 }, "Hello");
+
+            Console.WriteLine(result);
+        }
+    }
+}
