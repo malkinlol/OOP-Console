30b282330f5461c530e52b30761d20a6ddce2f13
Eduard Popkov <E.Papkou@sam-solutions.com>
2019-12-23 17:25:53 +0300

    feat: Lab2_Option1 High_Level

---
+++ Lab2_Option1/High_Level/Classes/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace High_Level.Classes
+{
+    public class Person : IDisposable
+    {
+        public string name;
+        public int age;
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Disposed");
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {this.name}" + Environment.NewLine +
+                $"Age: {this.age}";
+        }
+    }
+}
---
+++ Lab2_Option1/High_Level/Program.cs
@@ -0,0 +1,36 @@
+﻿using High_Level.Classes;
+using System;
+
+namespace High_Level
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task1_Option1();
+        }
+
+        private static void Task1_Option1()
+        {
+            using(Person person = new Person("Eduard", 20))
+            {
+                Console.WriteLine("Get name: {0}", person.name);
+            }
+
+            Console.WriteLine("Method Task1 ended!");
+        }
+
+        private static void Task1_Option2()
+        {
+            ///
+            /// You can try to run this code if you have C# 8 
+            /// 
+            
+            //using Person person = new Person("Arina", 19);
+
+            //Console.WriteLine("Get name: {0}", person.name);
+
+            //Console.WriteLine("Method Task1 ended!");
+        }
+    }
+}
